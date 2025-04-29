9c26bad3486c0ec7b268ac024537a6c41fab4e80
Gabriel Carneiro Jr <gacarneirojr@hotmail.com>
2025-04-29 13:51:34 -0300

    fix: remove unnecessary regex anchors

--- nopassword/urls.py
+++ nopassword/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from nopassword import views
 
 urlpatterns = [
-    path(r'^login/$', views.LoginView.as_view(), name='login'),
-    path(r'^login/code/$', views.LoginCodeView.as_view(), name='login_code'),
-    path(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    path(r'login/', views.LoginView.as_view(), name='login'),
+    path(r'login/code/', views.LoginCodeView.as_view(), name='login_code'),
+    path(r'logout/', views.LogoutView.as_view(), name='logout'),
 ]
